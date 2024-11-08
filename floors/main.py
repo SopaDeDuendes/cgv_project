53091c26786706ef562b6b6ebb6380700dd26c69
Erick L <ericklc1255@gmail.com>
2024-11-08 16:28:36 -0500

    layout changes

--- floors/main.py
+++ floors/main.py
@@ -11,16 +11,13 @@ class OpenGLWidget(QGLWidget):
         self.current_floor = 4  # Iniciar en el piso 5 
 
     def set_floor(self, floor_index):
-        """Cambia el piso actual y renderiza el nuevo piso."""
         self.current_floor = floor_index
         self.update() 
 
     def initializeGL(self):
-        """Inicialización de OpenGL (se ejecuta una vez)."""
         self.setFixedSize(1280 , 720) 
 
     def paintGL(self):
-        """Función para renderizar usando OpenGL."""
         render_floor_with_people(self.current_floor)
 
 class MainWindow(QMainWindow):
@@ -31,28 +28,10 @@ class MainWindow(QMainWindow):
         self.total_floors = 5
 
         self.opengl_widget = OpenGLWidget(self)
-
-        # Label que muestra en qué piso estamos
-        self.floor_label = QLabel(f"Piso Actual: {self.current_floor + 1}", self)
-        self.floor_label.setAlignment(Qt.AlignCenter)
-
-        # Botones para desplazarse entre pisos
-        self.prev_button = QPushButton('⬅ Piso Anterior')
-        self.next_button = QPushButton('Piso Siguiente ➡')
-
-        self.prev_button.clicked.connect(self.prev_floor)
-        self.next_button.clicked.connect(self.next_floor)
-
-        # Layout horizontal para los botones a los costados
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self.prev_button)
-        button_layout.addWidget(self.floor_label)
-        button_layout.addWidget(self.next_button)
-
+    
         # Layout principal vertical que incluye OpenGL y los botones
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.opengl_widget)
-        main_layout.addLayout(button_layout)
 
         # Widget principal
         container = QWidget()
@@ -64,25 +43,11 @@ class MainWindow(QMainWindow):
         self.timer.timeout.connect(self.update_simulation)
         self.timer.start(100)  # Bucle de 100 ms
 
-    def prev_floor(self):
-        """Función para ir al piso anterior."""
-        if self.current_floor > 0:
-            self.current_floor -= 1
-            self.update_display()
-
-    def next_floor(self):
-        """Función para ir al piso siguiente."""
-        if self.current_floor < self.total_floors - 1:
-            self.current_floor += 1
-            self.update_display()
-
     def update_display(self):
-        """Actualiza el label y el widget OpenGL para reflejar el piso actual."""
         self.floor_label.setText(f"Piso Actual: {self.current_floor + 1}")
-        self.opengl_widget.set_floor(self.current_floor)  # Actualiza el piso en OpenGL
+        self.opengl_widget.set_floor(self.current_floor)  
 
     def update_simulation(self):
-        """Función que fuerza el redibujado de OpenGL cada vez que se actualiza."""
         self.opengl_widget.update()
 
 def start_simulation():
